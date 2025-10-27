ed937fe15f9e5b7917d282f8454214bdf7f650fa
dagnyr <dagnyreese@icloud.com>
2025-10-27 11:03:32 -0700

    working more on testing edges

--- test/graphstesting.jl
+++ test/graphstesting.jl
@@ -4,7 +4,7 @@ g = MetaGraph(
     Graph();
     label_type=Symbol,
     vertex_data_type=Vector{String},  # test storing multiple labels?
-    edge_data_type=Tuple{String,Int},
+    edge_data_type=Vector{String},
 )
 
 # plans?
@@ -14,4 +14,35 @@ g = MetaGraph(
 # maybe store vector of dictionary with String, Any so I can store label followed by thing?
 # vertices will store
 
-vertex_data_type = Dict{Symbol,Any}
+#vertex_data_type = Dict{Symbol,Any}
+
+add_vertex!(g, :Owo, ["hey", "hello", "hi"])
+add_vertex!(g, :Uwu, ["goodbye", "seeya", "bye"])
+
+add_edge!(g, :Uwu, :Owo, ["tidings", "bonjour", "nihao"])
+
+edge_data = g[:Uwu, :Owo]
+push!(edge_data, "shalom")
+g[:Uwu, :Owo] = edge_data
+
+g[:Uwu, :Owo]
+
+
+# now trying with dictionary
+# Dict{Symbol,Any}
+
+g = MetaGraph(
+    Graph();
+    label_type=Symbol,
+    vertex_data_type=Dict{Symbol,Any},  # test storing multiple labels?
+    edge_data_type=Dict{Symbol,Any},
+)
+
+add_vertex!(g, :Owo, Dict(:label => "ACGATAG", :vgene => "V13B")) # cdr3 and gene is fake for testing
+add_vertex!(g, :Uwu, Dict(:label => "CCGGTAT", :vgene => "V12A"))
+
+add_edge!(g, :Uwu, :Owo, Dict(:type => "local", :pval => 0.02))
+add_edge!(g, :Uwu, :Owo, Dict(:type => "global", :distance => 2)) # replaces prior edge
+
+# solution? add a vector of dicionaries to edges and have a global distance, global p-val, motif and motif p-val entry.
+# to determine if theres a global or local edge use bool to check for each edge?
